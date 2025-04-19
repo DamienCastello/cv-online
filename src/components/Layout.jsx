eb5ac70380f378d7935f39e2264b949eb308454e
Gamma <d.castello.13200@gmail.com>
2025-04-19 18:44:03 +0200

    master(base): add burger menu and fix responsive

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,20 @@
 import { Outlet } from 'react-router-dom'
 import Sidebar from './Sidebar'
 import '../styles/layout.css'
+import { RxHamburgerMenu } from "react-icons/rx";
+import { useState } from 'react';
+
 
 const Layout = () => {
+  const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarVisible(!isSidebarVisible);
+  };
   return (
-    <div className="layout">
-      <Sidebar />
+    <div className={`layout ${!isSidebarVisible ? 'sidebar-hidden' : 'sidebar-visible'}`}>
+      { isSidebarVisible ? <Sidebar toggleSidebar={toggleSidebar} /> : <RxHamburgerMenu className='burger-menu' onClick={toggleSidebar} aria-label="Toggle sidebar"/>}
+      
       <main className="content">
         <Outlet />
       </main>
